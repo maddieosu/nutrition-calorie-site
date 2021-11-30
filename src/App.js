a86db5d0f58751696a2e369a98f13aab80ceffc6
Blake Hakkila <blake.austin.hakkila@gmail.com>
2021-11-29 17:42:45 -0800

    Added CSS styling and formatted advice text better

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function fetchNutritionData(query) {
 }
 
 function App() {
-  const [adviceParagraph, setAdvice] = useState();
   const [height, setHeight] = useState();
   const heightInput = useRef();
   const [weight, setWeight] = useState();
@@ -27,16 +26,7 @@ function App() {
   const goalInput = useRef();
   const [nutritionQuery, setNutritionQuery] = useState();
   const nutritionInput = useRef();
-
   
-  /*
-  const handleChange = e => {
-    const target = e.target;
-    if (target.checked) {
-      setSex(target.value);
-    }
-  };
-  */
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -45,7 +35,9 @@ function App() {
     heightInput.current.value = null;
     weightInput.current.value = null;
     goalInput.current.value = null;
-    setAdvice("Your height is " + height + "\nYour weight is " + weight + "\nYour sex is " + sex + ".\nYou are " + (weight - goal) + " lbs away from your goal. Your recommended calories per day is " + calories + " to reach your goal weight in " + Math.ceil(weight/4) + " month(s)"); 
+    let adviceHTML = document.getElementById("advice");
+    let adviceParagraph = "Your height is " + height + ".<br>Your weight is " + weight + ".<br>Your sex is " + sex + ".<br>You are " + (weight - goal) + " lbs away from your goal.<br>Your recommended calories per day is " + calories + " to reach your goal weight in " + Math.ceil(weight/4) + " month(s).";
+    adviceHTML.innerHTML = adviceParagraph;
   };
 
   const handleNutritionSearch = e => {
@@ -69,10 +61,10 @@ function App() {
           1258: "Saturated Fat",
         }
         let foodSelection = result.foods[0];
-        let nutrientInfo = {"Serving Size": `Serving Size: ${foodSelection.servingSize} ${foodSelection.servingSizeUnit.toLowerCase()}<br>`};
+        let nutrientInfo = {"Serving Size": `Serving Size: ${Math.round(10*foodSelection.servingSize)/10} ${foodSelection.servingSizeUnit.toLowerCase()}<br>`};
         for (const entry of foodSelection.foodNutrients) {
           let nutrientName = nutritionIdMap[entry.nutrientId];
-          nutrientInfo[nutrientName] = `${nutrientName}: ${entry.value} ${entry.unitName.toLowerCase()}<br>`;
+          nutrientInfo[nutrientName] = `${nutrientName}: ${Math.round(10*entry.value)/10} ${entry.unitName.toLowerCase()}<br>`;
         }
         let nutritionDataString = `${nutritionQuery} has the following nutritional content<br>`;
         let nutritionOrder = ["Serving Size", "Energy", "Total Fat", "Saturated Fat", "Cholesterol", "Sodium", "Total Carbohydrates", "Total Sugars", "Added Sugar", "Protein"];
@@ -96,39 +88,40 @@ function App() {
   return (
     <div className="App">
       <div id="header">
-        <h1>Reach Your Fitness Goal</h1>
-        <p>Just enter some basic information</p>
+        <h1 id="Reach">Reach Your Fitness Goal</h1>
+        <p id="Just">Just enter some basic information</p>
       </div>
 
       <form>
-        Height: 
+        Height: &nbsp;
         <input ref={heightInput} onChange={(e) => setHeight(e.target.value)} type="text" id="height" name="height" />
       </form>
       <form>
-        Weight: 
+        Weight: &nbsp;
         <input ref={weightInput} onChange={(e) => setWeight(e.target.value)} type="text" id="weight" name="weight" />
       </form>
       <form>
         <p id="">Sex</p>
         <input ref={isMaleInput} onChange={(e) => setSex(e.target.value)} type="radio" id="maleS" name="sex" value="Male" />
-        <label for="maleS">Male</label><br />
+        <label for="maleS"> Male</label><br />
         <input ref={isFemaleInput} onChange={(e) => setSex(e.target.value)} type="radio" id="femaleS" name="sex" value="Female" />
-        <label for="femaleS">Female</label><br />
+        <label for="femaleS"> Female</label><br />
       </form>
       <form>
-        Weight Goal: 
+        Weight Goal: &nbsp;
         <input ref={goalInput} onChange={(e) => setGoal(e.target.value)} type="text" name="goal" /> lbs
       </form>
       <form>
-        <input onClick={handleSubmit} type="button" id="execute" value="Get my diet plan" />
+        <input onClick={handleSubmit} type="button" id="execute" value="GET MY DIET PLAN" />
       </form>
-      <p id="advice">{adviceParagraph}</p>
+      <p id="advice"></p>
+      <br />
       <form>
-        Search nutrition data:
+        Search nutrition data: &nbsp;
         <input ref={nutritionInput} onChange={(e) => setNutritionQuery(e.target.value)} type="text" name="nutrition" />
       </form>
       <form>
-        <input onClick={handleNutritionSearch} type="button" id="nutritionSubmit" value="Search for nutrition data" />
+        <input onClick={handleNutritionSearch} type="button" id="nutritionSubmit" value="SEARCH FOR NUTRITION DATA" />
       </form>
       <p id="nutritionData"></p>
     </div>
